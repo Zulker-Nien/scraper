338f886adb55249bfcfb4fe59f50343f7cbd117c
Zulker Nien <zulkerb9b@gmail.com>
2023-09-03 01:43:30 +0600

    Fix!: Getting links from all blogs

--- pageScraper.js
+++ pageScraper.js
@@ -6,11 +6,14 @@ const scraperObject = {
     // Navigate to the selected page
     await page.goto(this.url);
     // Wait for the required DOM to be rendered
-    await page.waitForSelector(".page_inner");
+    await page.waitForSelector(".header");
     // Get the link to all the required books
-    let urls = await page.$$eval(".stories-set", (links) => {
-      // Extract the links from the data
-      links = links.map((el) => el.querySelector(".news-with-item > a").href);
+    let urls = await page.evaluate(() => {
+      const links = [];
+      const linkElements = document.querySelectorAll(".stories-set a");
+      linkElements.forEach((el) => {
+        links.push(el.href);
+      });
       return links;
     });
     console.log(urls);
@@ -18,3 +21,25 @@ const scraperObject = {
 };
 
 module.exports = scraperObject;
+// let pagePromise = (link) =>
+//   new Promise(async (resolve, reject) => {
+//     let dataObj = {};
+//     let newPage = await browser.newPage();
+//     await newPage.goto(link);
+//     dataObj["blogTitle"] = await newPage.$eval(
+//       ".story-title-info > h1",
+//       (text) => text.textContent
+//     );
+//     dataObj["blogContent"] = await newPage.$eval(
+//       ".story-title-info > h1",
+//       (text) => text.textContent
+//     );
+//     resolve(dataObj);
+//     await newPage.close();
+//   });
+
+// for (link in urls) {
+//   let currentPageData = await pagePromise(urls[link]);
+//   // scrapedData.push(currentPageData);
+//   console.log(currentPageData);
+// }
